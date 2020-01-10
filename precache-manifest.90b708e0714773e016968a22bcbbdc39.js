f1b8bf31e278944822a47bf745c45b43b033c44b
Stefan Francis <stefan.mec.93@gmail.com>
2020-01-10 20:30:23 +0530

    Updates

---
+++ precache-manifest.90b708e0714773e016968a22bcbbdc39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b66103cef583795aa099d280d43f3b9",
+    "url": "/story-board/index.html"
+  },
+  {
+    "revision": "5c67c06ea89ff52d33f0",
+    "url": "/story-board/static/css/main.fc10fb17.chunk.css"
+  },
+  {
+    "revision": "cf296ee7ea028396ba08",
+    "url": "/story-board/static/js/2.7daa45bf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/story-board/static/js/2.7daa45bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c67c06ea89ff52d33f0",
+    "url": "/story-board/static/js/main.bb274564.chunk.js"
+  },
+  {
+    "revision": "e7c58774a28ba7b96e44",
+    "url": "/story-board/static/js/runtime-main.86b025ee.js"
+  }
+]);
